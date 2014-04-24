669bf2d331f2b5eaab37b00475aaaae1d615dbf0
Florian Specker <florian.specker@spontacts.com>
2014-04-24 18:16:00 +0200

    added content

--- src/main/java/ch/conceptworks/mooris_playground/ListActivity.java
+++ src/main/java/ch/conceptworks/mooris_playground/ListActivity.java
@@ -8,6 +8,8 @@ package ch.conceptworks.mooris_playground;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class ListActivity extends Activity {
@@ -15,14 +17,20 @@ public class ListActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.list_layout);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        TextView textView = (TextView) findViewById(R.id.text_view);
+        TextView textView = (TextView) findViewById(R.id.text_view1);
         textView.setText("Hello world!");
+        textView = (TextView) findViewById(R.id.text_view3);
+        textView.setText("Another title");
     }
 
 }
